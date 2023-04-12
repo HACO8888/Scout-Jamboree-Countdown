abd36293d84636c32256c70b787427eeff21b11a
HACO <jasonytonlinecomeandsee@gmail.com>
2023-04-12 20:53:56 +0800

    Update

---
+++ src/components/FinishTimer.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const Timer = () => {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const deadline = "August 12, 2023";
+
+  const getTime = () => {
+    const time = Date.parse(deadline) - Date.now();
+
+    setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+    setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+    setMinutes(Math.floor((time / 1000 / 60) % 60));
+    setSeconds(Math.floor((time / 1000) % 60));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => getTime(deadline), 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+      <span className="text-5xl font-bold">
+        {days < 10 ? "0" + days : days}天 {hours < 10 ? "0" + hours : hours}時 {minutes < 10 ? "0" + minutes : minutes}分 {seconds < 10 ? "0" + seconds : seconds}秒
+      </span>
+  );
+};
+
+export default Timer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors : {
+        "purple": '#6E20A0',
+        "purple-light": '#A44AF2',
+        "purple-dark": '#3D0A6F',
+        "white": '#FFFFFF',
+      }
     },
   },
   plugins: [],
